364179c21b8e5ef5b86f8773de6c83dbe3b39230
Ashley Davis <ashley@codecapers.com.au>
2023-01-28 12:33:25 +1000

    Added the frontend originally created in the repo photosphere-frontend.

---
+++ frontend/src/app.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import { GalleryPage } from "./pages/gallery";
+import { UploadPage } from "./pages/upload";
+
+export function App() {
+    return (
+        <BrowserRouter>
+            <div className="nav">
+                <div className="title">Photosphere</div>
+                <Link to="/" className="link">Home</Link>
+                <Link to="/upload" className="link">Upload</Link>
+            </div>
+            <Routes>
+                <Route 
+                    path="/" 
+                    element={<GalleryPage />} 
+                    />
+                <Route 
+                    path="/upload" 
+                    element={<UploadPage />} 
+                    />
+           </Routes>
+        </BrowserRouter>
+    );
+}
