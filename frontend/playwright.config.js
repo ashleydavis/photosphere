a74f232e4b74be75dd96f42d53c8bbdc5f778848
Ashley Davis <ashley@codecapers.com.au>
2023-01-29 20:51:36 +1000

    Double the timeout to get my test to run in the CI pipeline.

--- frontend/playwright.config.js
+++ frontend/playwright.config.js
@@ -12,7 +12,7 @@ import { devices } from '@playwright/test';
 const config = {
   testDir: './src/test/e2e',
   /* Maximum time one test can run for. */
-  timeout: 30 * 1000,
+  timeout: 120 * 1000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
