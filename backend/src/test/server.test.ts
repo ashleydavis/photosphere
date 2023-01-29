ca10b8df5c3bfb931df34ccd62fa430e6d0a1985
Ashley Davis <ashley@codecapers.com.au>
2023-01-29 11:41:36 +1000

    Added a test for uploading an asset.

--- backend/src/test/server.test.ts
+++ backend/src/test/server.test.ts
@@ -1,5 +1,7 @@
 import { createServer } from "../server";
 import request from "supertest";
+import * as fs from "fs";
+import { ObjectId } from "mongodb";
 
 describe("photosphere backend", () => {
 
@@ -23,18 +25,61 @@ describe("photosphere backend", () => {
         };
 
         const app = createServer(mockDb);
-        return app;
+        return { 
+            app, 
+            mockCollection, 
+            mockDb,
+        };
     }
-
 
     test("no assets", async () => {
 
-        const app = initServer();
+        const { app } = initServer();
         const response = await request(app).get("/assets");
         
         expect(response.statusCode).toBe(200);
         expect(response.body).toEqual({ 
             assets: [],
+        });
+    });
+
+    test("upload asset", async () => {
+
+        const { app, mockCollection } = initServer();
+
+        mockCollection.insertOne = jest.fn();
+
+        const fileName = "a-test-file.jpg";
+        const contentType = "image/jpg";
+        const width = 256;
+        const height = 1024;
+        const hash = "1234";
+
+        const response = await request(app)
+            .post("/asset")
+            .set("file-name", fileName)
+            .set("content-type", contentType)
+            .set("width", width.toString())
+            .set("height", height.toString())
+            .set("hash", hash)
+            .send(fs.readFileSync("./test/test-assets/1.jpeg"));
+
+        const assetId = response.body.assetId;
+
+        expect(response.statusCode).toBe(200);
+        expect(assetId).toBeDefined();
+        expect(assetId.length).toBeGreaterThan(0);
+
+        expect(mockCollection.insertOne).toHaveBeenCalledTimes(1);
+        expect(mockCollection.insertOne).toHaveBeenCalledWith({
+            _id: new ObjectId(assetId),
+            origFileName: fileName,
+            contentType: contentType,
+            src: `/asset?id=${assetId}`,
+            thumb: `/asset?id=${assetId}`,
+            width: width,
+            height: height,
+            hash: hash,
         });
     });
 });
