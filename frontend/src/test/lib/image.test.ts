7a5c4925eb4e0d98ffa6d591808b0e965329e0b0
Ashley Davis <ashley@codecapers.com.au>
2023-01-31 15:26:55 +1000

    Now testing loading and getting resolution for an inline image.

--- frontend/src/test/lib/image.test.ts
+++ frontend/src/test/lib/image.test.ts
@@ -2,15 +2,26 @@
  * @jest-environment-options { "resources": "usable" }
  */
 
-import { loadImage } from "../../lib/image";
+import { getImageResolution, loadImage } from "../../lib/image";
 
 describe("image", () => {
 
+    // const src = "https://codecapers.com.au/assets/img/profile.jpg";
+    const testImg = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAIAAAACUFjqAAAAXUlEQVR4nL3MoRHAIAxG4bQDRJGLYP9tmAEwiQHFAPy4Co5W9tnv7l0pJXrv/rCfuPdeSiGiWmtrbecQAoCc85xTRA5zVR1jqOphDsDMYoxmBmBnd2dmEWFmd394AV5LK0bYIwU3AAAAAElFTkSuQmCC";
+
     test("can load image", async () => {
 
-        const src = "https://codecapers.com.au/assets/img/profile.jpg";
-        const image = await loadImage(src);
-        expect(image.src).toBe(src);
+        const image = await loadImage(testImg);
+        expect(image.src).toBe(testImg);
+    });
+
+    test("can get image resolution", async () => {
+
+        const resolution = await getImageResolution(testImg);
+        expect(resolution).toEqual({
+            width: 10,
+            height: 10,
+        });
     });
 
 });
