89bd566368c2dcc1d9f45f48bd97f238ebee7698
Ashley Davis <ashley@codecapers.com.au>
2023-01-31 15:53:22 +1000

    Tested the function for loading a file.

--- frontend/src/lib/image.ts
+++ frontend/src/lib/image.ts
@@ -3,11 +3,11 @@
 //
 // https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/Data_URLs
 //
-export function loadFile(file: File): Promise<string> { 
+export function loadFile(file: File | Blob): Promise<string> { 
     return new Promise((resolve, reject) => {
         const reader = new FileReader();
         reader.addEventListener('error', () => {
-            reject(new Error(`Error reading file ${file.name}.`));
+            reject(new Error(`Error reading file.`));
         });
 
         reader.addEventListener('load', evt => {
--- frontend/src/test/lib/image.test.ts
+++ frontend/src/test/lib/image.test.ts
@@ -2,12 +2,14 @@
  * @jest-environment-options { "resources": "usable" }
  */
 
-import { getImageResolution, loadImage } from "../../lib/image";
+import { getImageResolution, loadFile, loadImage } from "../../lib/image";
+import { base64StringToBlob } from 'blob-util';
 
 describe("image", () => {
 
-    // const src = "https://codecapers.com.au/assets/img/profile.jpg";
-    const testImg = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAIAAAACUFjqAAAAXUlEQVR4nL3MoRHAIAxG4bQDRJGLYP9tmAEwiQHFAPy4Co5W9tnv7l0pJXrv/rCfuPdeSiGiWmtrbecQAoCc85xTRA5zVR1jqOphDsDMYoxmBmBnd2dmEWFmd394AV5LK0bYIwU3AAAAAElFTkSuQmCC";
+    const contentType = "image/png";
+    const base64Data = "iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAIAAAACUFjqAAAAXUlEQVR4nL3MoRHAIAxG4bQDRJGLYP9tmAEwiQHFAPy4Co5W9tnv7l0pJXrv/rCfuPdeSiGiWmtrbecQAoCc85xTRA5zVR1jqOphDsDMYoxmBmBnd2dmEWFmd394AV5LK0bYIwU3AAAAAElFTkSuQmCC";
+    const testImg = `data:${contentType};base64,${base64Data}`;
 
     test("can load image", async () => {
 
@@ -22,6 +24,13 @@ describe("image", () => {
             width: 10,
             height: 10,
         });
+    });
+
+    test("can load file", async () => {
+
+        const blob = base64StringToBlob(base64Data, contentType);
+        const loadedFile = await loadFile(blob);
+        expect(loadedFile).toEqual(testImg);
     });
 
 });
