bbca102cc53375a393c47664ec2832232b64525e
Ashley Davis <ashley@codecapers.com.au>
2023-02-09 18:26:42 +1000

    Factored out a "main" component so that I can have it under the BrowserRouter and use React Router's "navigate" hook.
    Now I can procedurally navigate between pages, for example here opening the search input navigates to the cloud page.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,15 +2,7 @@ import React from "react";
 import { createRoot } from 'react-dom/client';
 import { App } from "./app";
 import '@fortawesome/fontawesome-free/css/all.css';
-import { ApiContextProvider } from "./context/api-context";
-import { GalleryContextProvider } from "./context/gallery-context";
 
 const container = document.getElementById('app');
 const root = createRoot(container!);
-root.render(
-    <ApiContextProvider>
-        <GalleryContextProvider>
-            <App />
-        </GalleryContextProvider>
-    </ApiContextProvider>
-);
+root.render(<App />);
