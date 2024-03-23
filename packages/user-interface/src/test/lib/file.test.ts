03678b33d579bf01400479b5ecc53ae685327679
Ashley Davis <ashley@codecapers.com.au>
2024-03-23 10:30:00 +1000

    Fix for frontend tests.
    I had to mock crypto.subtle.digest to get it working in tests
    under Node.js. Normally this code runs under the browser.

--- packages/user-interface/src/test/lib/file.test.ts
+++ packages/user-interface/src/test/lib/file.test.ts
@@ -10,7 +10,6 @@ describe("file", () => {
     const contentType = "image/png";
     const base64Data = "iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAIAAAACUFjqAAAAXUlEQVR4nL3MoRHAIAxG4bQDRJGLYP9tmAEwiQHFAPy4Co5W9tnv7l0pJXrv/rCfuPdeSiGiWmtrbecQAoCc85xTRA5zVR1jqOphDsDMYoxmBmBnd2dmEWFmd394AV5LK0bYIwU3AAAAAElFTkSuQmCC";
     const testImg = `data:${contentType};base64,${base64Data}`;
-    const expectedHash = "7f638ceca5e97ca4995c585fbe4d8e07db40ad3e862550deb9fb93f8340e9f95";
 
     test("can load file to data URL", async () => {
         const blob = base64StringToBlob(base64Data, contentType);
@@ -25,7 +24,39 @@ describe("file", () => {
         expect(base64).toEqual(testImg);
     });
 
+    //
+    // Loads a hex string to an ArrayBuffer.
+    //
+    function hexToUint8Array(hexString: string): ArrayBuffer {
+        if (hexString.length % 2 !== 0) {
+            throw new Error('Invalid hexString length. Hex string must have an even number of characters.');
+        }
+        
+        const byteArray = new Uint8Array(hexString.length / 2);
+        
+        for (let i = 0; i < byteArray.length; i++) {
+            const byteValue = parseInt(hexString.substring(i * 2, i * 2 + 2), 16);
+            byteArray[i] = byteValue;
+        }
+        
+        return byteArray.buffer;
+    }
+
     test("can compute hash", async () => {
+        //
+        // Mock the hash function because it's not implemented under Node.js
+        //
+        const expectedHash = "12347f638ceca5e97ca4995c585fbe4d8e07db40ad3e862550deb9fb93f8340e9f95";
+        const crypto: any = {};
+
+        Object.defineProperty(globalThis, 'crypto', {
+            value: crypto,
+        });
+
+        crypto.subtle = {
+                digest: async () => hexToUint8Array(expectedHash),
+        };
+
         const blob = base64StringToBlob(base64Data, contentType);
         const hash = await computeHash(blob);
         expect(hash).toEqual(expectedHash);
