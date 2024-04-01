5abba7392d8b2a447d552d6a8a60e120e4d10f04
Ashley Davis <ashley@codecapers.com.au>
2024-04-01 16:35:34 +1000

    Removed a wierd empty string.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -228,7 +228,7 @@ export async function createServer(now: () => Date, assetDatabase: IAssetDatabas
         });
 
         assetStream.stream.pipe(res);
-``    }));
+    }));
 
     //
     // Uploads a display version for a particular asset.
