88df0ddbf5ee4ef54f5f93f90aa7f756e9921204
Ashley Davis <ashley@codecapers.com.au>
2024-05-08 16:10:10 +1000

    The service worker now only caches static content.
    REST API requests that return JSON are ignored because this is not
    content that should be cache.
    
    Images are also not cached because they have a separate local storage
    system in the main app.

--- frontend/src/service-worker.ts
+++ frontend/src/service-worker.ts
@@ -10,6 +10,13 @@
 /// <reference lib="ES2017" />
 /// <reference lib="webworker" />
 
+const cacheableContentTypes = [
+    "text/html",
+    "text/css",
+    "application/javascript",
+    "text/javascript",
+];
+
 self.addEventListener('fetch', (event: any) => {
 
     // Bug fix
@@ -18,21 +25,28 @@ self.addEventListener('fetch', (event: any) => {
         return;
     }
 
-    event.respondWith(
-        fetch(event.request)
-            .then(response => {    
-                // Create a copy of the response and save it to the cache.
-                let copy = response.clone();
-                event.waitUntil(caches.open('app')
-                    .then(cache => {
-                        return cache.put(event.request, copy);
-                    }));
-    
-                // Return the response
-                return response;    
-            })
-            .catch(error => {
-                return caches.match(event.request);
-            })
-    );    
+    // console.log(`>> HTTP ${event.request.method} ${event.request.url}`);
+    // console.log(`Requesting asset type ${event.request.headers.get('Accept')}.`);
+
+    for (const contentType of cacheableContentTypes) {
+        if (event.request.headers.get('Accept')?.includes(contentType)) {
+            event.respondWith(
+                fetch(event.request)
+                    .then(response => {    
+                        // Create a copy of the response and save it to the cache.
+                        let copy = response.clone();
+                        event.waitUntil(caches.open('app')
+                            .then(cache => {
+                                return cache.put(event.request, copy);
+                            }));
+            
+                        // Return the response
+                        return response;    
+                    })
+                    .catch(error => {
+                        return caches.match(event.request);
+                    })
+            );    
+        }
+    }
 });    
