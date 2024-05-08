b4e730de60d45ca283c4c13996559ed1ba4d4ab8
Ashley Davis <ashley@codecapers.com.au>
2024-05-08 11:42:10 +1000

    Added a testbed version of the frontend.
    This can be used to test all sorts of things under the browser
    that don't work under regular Jest tests.

---
+++ testbed/README.md
@@ -0,0 +1,24 @@
+# Testbed 
+
+A testbed version of the frontend.
+
+This can be used to test all sorts of things under the browser
+that don't work under regular Jest tests.
+
+## Setup
+
+Open a terminal and change directory to the testbed project:
+
+```bash
+cd testbed
+```
+
+## Run the Webpack dev server
+
+Run the dev server like this:
+
+```bash
+pnpm start
+```
+
+Then click the link or point your browser at the default location for Webpack: [http://localhost:8080](http://localhost:8080).
---
+++ testbed/webpack.config.js
@@ -0,0 +1,63 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.tsx',
+    devtool: 'inline-source-map',
+    mode: 'development',
+    target: 'web',
+    performance: { //TODO: Adjust/remove this later for prod performance.
+        hints: false,
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000
+    },
+    devServer: {
+        historyApiFallback: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'postcss-loader',
+                ],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+        // https://stackoverflow.com/a/64802753/25868
+        fallback: {
+            "fs": false,
+            "tls": false,
+            "net": false,
+            "path": false,
+            "zlib": false,
+            "http": false,
+            "https": false,
+            "stream": false,
+            "crypto": false,
+            "crypto-browserify": require.resolve('crypto-browserify'),
+        },
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new webpack.EnvironmentPlugin({
+            ...process.env,
+        }),
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+        }),
+    ],
+};
