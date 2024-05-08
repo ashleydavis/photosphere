29bc1a891610417291055439deed967adaaf0917
Ashley Davis <ashley@codecapers.com.au>
2024-05-08 13:51:53 +1000

    Added a test that indexeddb records can be retreived in expected order.

--- testbed/src/tests/test-indexeddb.tsx
+++ testbed/src/tests/test-indexeddb.tsx
@@ -1,6 +1,22 @@
 import React, { useEffect } from "react";
 import { indexeddb } from "user-interface";
-import { storeRecord } from "user-interface/build/lib/indexeddb";
+
+//
+// Checks for equality between two arrays.
+//
+function arraysEqual<T>(a: T[], b: T[]): boolean {
+    if (a.length !== b.length) {
+        return false;
+    }
+
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] !== b[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
 
 export function TestIndexeddb() {
 
@@ -8,23 +24,24 @@ export function TestIndexeddb() {
 
         let db: IDBDatabase | undefined = undefined;
 
+        
         async function testDb() {
 
             await indexeddb.deleteDatabase("photosphere-test-db");
-
+            
             db = await indexeddb.openDatabase("photosphere-test-db", 1, [
                 "test-collection",
             ]);
 
             const testRecords = [
+                { _id: "02", name: "test-2" },
+                                
                 //
                 // This record is considered the oldest because the ids are 
                 // in reverse chronological order.
                 //
                 { _id: "03", name: "test-3" }, 
-
-                { _id: "02", name: "test-2" },
-
+                
                 { _id: "01", name: "test-1" },
             ]
 
@@ -38,6 +55,12 @@ export function TestIndexeddb() {
 
             if (!leastRecentRecord || leastRecentRecord._id !== "03") {
                 throw new Error("Expected 03 to be the least recent record.");
+            }
+
+            const allRecords = await indexeddb.getAllRecords<any>(db, "test-collection");
+            const allRecordIds = allRecords.map(record => record._id);
+            if (!arraysEqual(allRecordIds, ["01", "02", "03"])) {
+                throw new Error("Expected all records to be in ascending order.");
             }
         }
 
