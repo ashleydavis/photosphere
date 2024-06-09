1b6ac10ee1f127ef787611b1c816434575a0d3ea
Ashley Davis <ashley@codecapers.com.au>
2024-06-10 08:37:07 +1000

    Fixed local user access.
    It wasn't going to be loading the local user correctly after
    storing with the wrong id.

--- packages/user-interface/src/context/app-context.tsx
+++ packages/user-interface/src/context/app-context.tsx
@@ -74,10 +74,7 @@ export function AppContextProvider({ children }: IProps) {
                 // Store user locally for offline use.
                 //
                 const userDatabase = indexeddb.databases.database("user");
-                await userDatabase.collection("user").setOne({
-                    ...user,
-                    _id: "user",
-                });
+                await userDatabase.collection("user").setOne(user);
                 localStorage.setItem("userId", user._id);
                 setUser(user);
                 return;
