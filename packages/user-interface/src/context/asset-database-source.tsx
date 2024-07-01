782c5e53c48047b53d7d8d86edb6bde4abdaa8c7
Ashley Davis <ashley@codecapers.com.au>
2024-07-01 17:13:45 +1000

    Added tests for the initial-sync.

--- packages/user-interface/src/context/asset-database-source.tsx
+++ packages/user-interface/src/context/asset-database-source.tsx
@@ -17,6 +17,7 @@ import { syncOutgoing } from "../lib/sync/sync-outgoing";
 import { syncIncoming } from "../lib/sync/sync-incoming";
 import { ILastUpdateRecord } from "../lib/sync/last-update-record";
 import { IDatabase } from "../lib/database/database";
+import { initialSync } from "../lib/sync/initial-sync";
 
 const SYNC_POLL_PERIOD = 5000;
 
@@ -449,54 +450,5 @@ export function AssetDatabaseProvider({ children }: IAssetDatabaseProviderProps)
             {children}
         </GallerySourceContext.Provider>
     );
-}
-
-async function initialSync(database: IDatabase, setId: string, api: IApi, setAssets: (assets: IGalleryItem[]) => void): Promise<void> {
-    let assets = await database.collection<IAsset>("metadata").getAllByIndex("setId", setId);
-    if (assets.length > 0) {
-        setAssets(assets);
-    }
-    else {
-        //
-        // Records the time of the latest update for the set.
-        // This should be done before the initial sync to avoid missing updates.
-        //
-        const latestTime = await api.getLatestTime();
-
-        //
-        // Load the assets from the cloud into memory.
-        //
-        let skip = 0;
-        const pageSize = 1000;
-        while (true) {
-            const records = await api.getAll<IAsset>(setId, "metadata", skip, pageSize);
-            if (records.length === 0) {
-                // No more records.
-                break;
-            }
-
-            skip += pageSize;
-            assets = assets.concat(records);           
-            setAssets(assets);
-        }
-
-        if (latestTime !== undefined) {
-            //
-            // Record the latest time where updates were received.
-            //
-            database.collection<ILastUpdateRecord>("last-update").setOne({
-                _id: setId,
-                lastUpdateTime: latestTime,
-            });
-        }
-
-        //
-        // Save the assets to the local database.
-        //
-        const localCollection = database.collection("metadata");
-        for (const asset of assets) {
-            await localCollection.setOne(asset);
-        }
-    }
 }
 
