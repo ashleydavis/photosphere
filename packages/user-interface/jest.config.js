a75df6b67ed08c6ec422f2eda70da326f5c190eb
Ashley Davis <ashley@codecapers.com.au>
2024-11-15 10:50:26 +1000

    Disabled jsdom and tests that depend on it.
    jsdom depends on the canvas shim which I can't get to install on Ubuntu.
    
    Removing this now, but these tests are useful so would like to add them again later.

--- packages/user-interface/jest.config.js
+++ packages/user-interface/jest.config.js
@@ -1,7 +1,11 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
     preset: 'ts-jest',
-    testEnvironment: 'jsdom',
+    //
+    // This would allow the image tests to work, except jsdom depends on
+    // the canvas shim which I can't get to install on Ubuntu.
+    //
+    // testEnvironment: 'jsdom',
     modulePathIgnorePatterns: [
         "dist",
         "build",
