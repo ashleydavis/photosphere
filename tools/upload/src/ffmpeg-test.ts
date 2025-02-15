cb24eec7e1545709296d4d1a634e7bd7aba2f94c
Ashley Davis <ashley@codecapers.com.au>
2025-02-15 10:20:19 +1000

    A testbed for playing with ffmpeg.

---
+++ tools/upload/src/ffmpeg-test.ts
@@ -0,0 +1,24 @@
+//
+// Testbed for experimenting with ffmpeg.
+//
+// Use it like this:
+//
+//    npx ts-node srec/ffmpeg-test.ts
+//
+
+import { getVideoScreenshot } from "./lib/video";
+
+async function main() {
+    const videoPath = "z:\\photos\\Photo Library\\Surrey Bike Ride.wmv";
+
+    const screenshot = getVideoScreenshot(videoPath);
+    console.log(screenshot);
+}
+
+main()
+    .catch(err => {
+        console.error(`Failed with error:`);
+        console.error(err);
+        process.exit(1);
+    });
+
