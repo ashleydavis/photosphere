21e5ae15acbdab0a25b4a5a6bc04396588a56f6a
Ashley Davis <ashley@codecapers.com.au>
2025-03-25 17:57:34 +1000

    Now automatically adding slashes between prefix and path where necessary.

--- packages/storage/src/lib/storage-prefix-wrapper.ts
+++ packages/storage/src/lib/storage-prefix-wrapper.ts
@@ -4,31 +4,47 @@
 
 import { Readable } from "stream";
 import { IFileInfo, IListResult, IStorage } from "./storage";
+import { join } from "path";
 
 export class StoragePrefixWrapper implements IStorage {
 
     constructor(private storage: IStorage, private prefix: string) {
+        if (prefix === "") {
+            throw new Error("Prefix must not be empty.");            
+        }
+    }
+
+    //
+    // Make a full path using the prefix.
+    //
+    private makeFullPath(path: string): string {
+        if (this.prefix.endsWith(":")) {
+            return this.prefix + path
+        }
+        else {
+            return join(this.prefix, path);
+        }
     }
 
     //
     // List files in storage.
     //
     listFiles(path: string, max: number, next?: string): Promise<IListResult> {
-        return this.storage.listFiles(this.prefix + path, max, next);
+        return this.storage.listFiles(this.makeFullPath(path), max, next);
     }
 
     //
     // List directories in storage.
     //
     listDirs(path: string, max: number, next?: string): Promise<IListResult> {
-        return this.storage.listDirs(this.prefix + path, max, next);
+        return this.storage.listDirs(this.makeFullPath(path), max, next);
     }
 
     //
     // Returns true if the specified file exists.
     //
     fileExists(filePath: string): Promise<boolean> {
-        return this.storage.fileExists(this.prefix + filePath);
+        return this.storage.fileExists(this.makeFullPath(filePath));
     }
 
     //
@@ -42,7 +58,7 @@ export class StoragePrefixWrapper implements IStorage {
     // Gets info about a file.
     //
     info(filePath: string): Promise<IFileInfo | undefined> {
-        return this.storage.info(this.prefix + filePath);
+        return this.storage.info(this.makeFullPath(filePath));
     }
 
     //
@@ -50,35 +66,35 @@ export class StoragePrefixWrapper implements IStorage {
     // Returns undefined if the file doesn't exist.
     //
     read(filePath: string): Promise<Buffer | undefined> {
-        return this.storage.read(this.prefix + filePath);
+        return this.storage.read(this.makeFullPath(filePath));
     }
 
     //
     // Writes a file to storage.
     //
     write(filePath: string, contentType: string | undefined, data: Buffer): Promise<void> {
-        return this.storage.write(this.prefix + filePath, contentType, data);
+        return this.storage.write(this.makeFullPath(filePath), contentType, data);
     }
 
     //
     // Streams a file from stroage.
     //
     readStream(filePath: string): Readable {
-        return this.storage.readStream(this.prefix + filePath);
+        return this.storage.readStream(this.makeFullPath(filePath));
     }
 
     //
     // Writes an input stream to storage.
     //
     writeStream(filePath: string, contentType: string | undefined, inputStream: Readable, contentLength?: number): Promise<void> {
-        return this.storage.writeStream(this.prefix + filePath, contentType, inputStream, contentLength);
+        return this.storage.writeStream(this.makeFullPath(filePath), contentType, inputStream, contentLength);
     }
 
     //
     // Deletes the file from storage.
     //
     delete(filePath: string): Promise<void> {
-        return this.storage.delete(this.prefix + filePath);
+        return this.storage.delete(this.makeFullPath(filePath));
     }
 
     //
