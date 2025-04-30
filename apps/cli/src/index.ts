fe3d7f9e8f0e0be3eca55062359dd41a0cbda85b
Ashley Davis <ashley@codecapers.com.au>
2025-04-30 15:29:29 +1000

    Prototype of embedding the front end in the CLI tool.

--- apps/cli/src/index.ts
+++ apps/cli/src/index.ts
@@ -1,6 +1,30 @@
 import { program } from 'commander';
 import { version } from '../package.json';
 import express from 'express';
+import path from 'path';
+import fs from 'fs';
+import AdmZip from "adm-zip";
+import os from 'os';
+
+import pfe from  "../pfe.zip" with { type: "file" } ;
+
+//
+// Extract frontend code if doesn't exist.
+//
+const frontendPath = path.join(os.tmpdir(), "photosphere/frontend/v1");
+if (!fs.existsSync(frontendPath)) {
+    fs.mkdirSync(frontendPath, { recursive: true });
+
+    const zip = new AdmZip(fs.readFileSync(pfe));
+    zip.extractAllTo(frontendPath, true); //TODO: Could also just stream the contents without extracing it.
+
+    console.log(`Extracted frontend to ${frontendPath}.`);
+}
+else {
+    console.log(`Frontend already exists at ${frontendPath}.`);
+}
+
+console.log(`Serving frontend from ${frontendPath}.`);
 
 async function main() {
 
@@ -17,7 +41,7 @@ async function main() {
             // Start an express server to serve static files.
             //
             const app = express();
-            app.use(express.static("public"));
+            app.use(express.static(path.join(frontendPath, "dist")));
             app.listen(3000, () => {
                console.log("Photosphere editor started at http://localhost:3000");
             });
