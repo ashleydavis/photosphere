ca756dc0d88ac33f77434b838bb4ce68038c0729
Ash <ashley@codecapers.com.au>
2025-07-05 08:35:36 +1000

    Removed the indent before the intro and outro.

--- apps/cli/src/lib/clack/prompts/messages.ts
+++ apps/cli/src/lib/clack/prompts/messages.ts
@@ -1,6 +1,6 @@
 import type { Writable } from 'node:stream';
 import color from 'picocolors';
-import { type CommonOptions, S_BAR, S_BAR_END, S_BAR_START } from './common';
+import { type CommonOptions, S_BAR_END } from './common';
 
 export const cancel = (message = '', opts?: CommonOptions) => {
 	const output: Writable = opts?.output ?? process.stdout;
@@ -9,10 +9,10 @@ export const cancel = (message = '', opts?: CommonOptions) => {
 
 export const intro = (title = '', opts?: CommonOptions) => {
 	const output: Writable = opts?.output ?? process.stdout;
-	output.write(`${color.gray(S_BAR_START)}  ${title}\n`);
+	output.write(`\n${title}\n`);
 };
 
 export const outro = (message = '', opts?: CommonOptions) => {
 	const output: Writable = opts?.output ?? process.stdout;
-	output.write(`${color.gray(S_BAR)}\n${color.gray(S_BAR_END)}  ${message}\n\n`);
+	output.write(`\n${message}\n\n`);
 };
