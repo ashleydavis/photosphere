8fe7c4152d1f41f6e6f89db521dc3aedc9de2765
Ash <ashley@codecapers.com.au>
2025-07-20 13:42:01 +1000

    Added a command to get the root hash of the database. It's useful for testing and debugging.

---
+++ apps/cli/src/cmd/root-hash.ts
@@ -0,0 +1,21 @@
+import { exit } from "node-utils";
+import { loadDatabase, IBaseCommandOptions } from "../lib/init-cmd";
+
+export interface IRootHashCommandOptions extends IBaseCommandOptions {
+}
+
+//
+// Command that prints just the root hash of the Photosphere media file database.
+//
+export async function rootHashCommand(options: IRootHashCommandOptions): Promise<void> {
+    
+    const { database } = await loadDatabase(options.db, options);
+
+    // Get database summary information
+    const summary = await database.getDatabaseSummary();
+
+    // Print only the root hash
+    console.log(summary.fullHash);
+
+    await exit(0);
+}
