29cde45d65a9852f295868355082a3bacb09cc5c
Ash <ashley@codecapers.com.au>
2025-08-02 10:03:06 +1000

    cli: Improved validation around image resize and other use of 3rd party tools.

--- packages/node-utils/src/lib/exec.ts
+++ packages/node-utils/src/lib/exec.ts
@@ -14,9 +14,25 @@ export function exec(command: string): Promise<{ stdout: string; stderr: string 
 //
 // Runs the command and adds logging for the tool used.
 //
-export async function execLogged(tool: string, command: string): Promise<{ stdout: string; stderr: string }> {
+export async function execLogged(tool: string, command: string, validate?: () => Promise<string | undefined>): Promise<{ stdout: string; stderr: string }> {
     log.verbose(`Executing ${tool} with command: "${command}"`);
-    const { stdout, stderr }  = await execAsync(command);
-    log.tool(tool, { stdout, stderr });
-    return { stdout, stderr };
+    try {
+        const { stdout, stderr } = await execAsync(command);
+        log.tool(tool, { stdout, stderr });
+        if (validate) {
+            const validationFailedReason = await validate();
+            if (validationFailedReason) {
+                const msg = `Validation failed for command: ${command}\nReason: ${validationFailedReason}`;
+                log.error(msg);
+                log.info(`===\nCommand: ${command}\nCommand stdout:\n${stdout ? stdout : 'No output'}\nCommand stderr:\n${stderr ? stderr : 'No error'}\n===`);
+                throw new Error(msg);
+            }
+        }
+        return { stdout, stderr };
+    }
+    catch (error: any) {
+        const msg = `Failed to execute command: ${command}\nError: ${error instanceof Error ? error.message : String(error)}`;
+        log.exception(msg, error);
+        throw new Error(msg);
+    }
 }
