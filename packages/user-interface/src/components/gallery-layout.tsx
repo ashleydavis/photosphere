dc3e9d97af6b630a385ca5c0d289b17211ae6eac
Ashley Davis <ashley@codecapers.com.au>
2025-09-22 07:40:12 +1000

    Added overrscan in the virtual viewport to make scrolling look smoother.

--- packages/user-interface/src/components/gallery-layout.tsx
+++ packages/user-interface/src/components/gallery-layout.tsx
@@ -191,7 +191,7 @@ export function GalleryLayout({ onItemClick }: IGalleryLayoutProps) {
         count: layout?.rows.length || 0,
         getScrollElement: () => containerRef.current,
         estimateSize: (i) => layout?.rows[i].height || 0,
-        overscan: 0,
+        overscan: 10,
     });
 
     //
