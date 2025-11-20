8c3c8de2604313b7368f00a1ebcb3e615eed823c
Ash <ashley@codecapers.com.au>
2025-11-21 07:46:30 +1000

    Fixed tests.

--- packages/task-queue/jest.config.js
+++ packages/task-queue/jest.config.js
@@ -1,9 +1,13 @@
 export default {
     preset: 'ts-jest',
     testEnvironment: 'node',
+    setupFiles: ['<rootDir>/__mocks__/worker-global.js'],
     modulePathIgnorePatterns: [
         "dist",
         "build",
     ],
+    moduleNameMapper: {
+        '^serialize-error$': '<rootDir>/__mocks__/serialize-error.js'
+    },
 };
 
