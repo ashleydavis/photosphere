40da6d57093f788560225056d7ccb99e7366b800
Ashley Davis <ashley@codecapers.com.au>
2026-01-01 18:21:37 +1000

    Removed service work code that I won't need with the move to local-first.

--- apps/frontend/src/index.tsx
+++ apps/frontend/src/index.tsx
@@ -9,18 +9,3 @@ const root = createRoot(container!);
 root.render(
     <App />
 );
-
-//
-// Register the service worker.
-//
-// https://css-tricks.com/add-a-service-worker-to-your-site/
-//
-if (navigator && navigator.serviceWorker) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('service-worker.js')
-            .catch(err => {
-                console.error(`Failed to register the service worker:`);
-                console.error(err);
-            });
-    });
-}
