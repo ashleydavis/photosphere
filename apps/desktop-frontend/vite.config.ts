8f7efc661a0ef58c4bbb98ea54903f94a1457b17
Ashley Davis <ashley@codecapers.com.au>
2026-01-06 17:58:19 +1000

    Changed the desktop frontend over to using Vite to bundle the code.
    I was already using Vite for the dev-frontend so this is the easiest way to get the desktop-frontend to build.

---
+++ apps/desktop-frontend/vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: './',
+  build: {
+    sourcemap: true,
+    minify: true,
+    outDir: '../desktop/bundle/frontend',
+    emptyOutDir: true,
+  },
+});
+
