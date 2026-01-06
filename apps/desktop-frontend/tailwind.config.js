e9f400f1b4faee3c94ac924e4378651c8e6afff0
Ashley Davis <ashley@codecapers.com.au>
2026-01-07 07:52:52 +1000

    Added Tailwind setup to desktop-frontend (from dev-frontend)

---
+++ apps/desktop-frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./src/**/*.{html,js,ts,jsx,tsx}",
+        "../../packages/user-interface/src/**/*.{html,js,ts,jsx,tsx}"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+
