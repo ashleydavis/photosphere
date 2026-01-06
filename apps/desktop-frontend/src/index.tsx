a4c75f5cd7e8193ccf5ef3af5c631503bc427afa
Ashley Davis <ashley@codecapers.com.au>
2026-01-07 07:54:43 +1000

    Added FontAwesome setup to desktop-frontend.

--- apps/desktop-frontend/src/index.tsx
+++ apps/desktop-frontend/src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
 import { App } from './app';
+import '@fortawesome/fontawesome-free/css/all.css';
 import './tailwind.css';
 
 const container = document.getElementById('root');
