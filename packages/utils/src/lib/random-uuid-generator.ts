375a7b61b4d60a1e8416dfbeb04fddab869127c4
Ashley Davis <ashley@codecapers.com.au>
2026-01-06 18:00:22 +1000

    Now using the uuid library which just works in the browser (as well as in Node.js).

--- packages/utils/src/lib/random-uuid-generator.ts
+++ packages/utils/src/lib/random-uuid-generator.ts
@@ -1,10 +1,12 @@
 import { IUuidGenerator } from './uuid-generator';
+import { v4 as uuidv4 } from 'uuid';
 
 //
-// Random UUID generator using crypto.randomUUID()
+// Random UUID generator using the uuid library
+// Works in both Node.js and browser environments
 //
 export class RandomUuidGenerator implements IUuidGenerator {
     generate(): string {
-        return crypto.randomUUID();
+        return uuidv4();
     }
 }
