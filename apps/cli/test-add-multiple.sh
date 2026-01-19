e5f125a9bc9b86a92f25ddffd8937708ecd7e603
Ashley Davis <ashley@codecapers.com.au>
2026-01-19 20:05:58 +1000

    Added a new test script to quickly add some assets to a new database.

---
+++ apps/cli/test-add-multiple.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Quick test script to create a database and add multiple files (test 7 assets)
+# This is useful for quickly testing the add functionality without running full smoke tests
+
+set -e
+
+TEST_DB_DIR="./test/tmp/test-add-multiple"
+MULTIPLE_IMAGES_DIR="../../test/multiple-images"
+
+# Delete test database if it exists
+echo "Deleting test database if it exists..."
+rm -rf "$TEST_DB_DIR"
+
+# Create new database
+echo "Creating new database at: $TEST_DB_DIR"
+bun run start -- init --db "$TEST_DB_DIR" --yes
+
+echo ""
+echo "Adding files from: $MULTIPLE_IMAGES_DIR"
+bun run start -- add --db "$TEST_DB_DIR" "$MULTIPLE_IMAGES_DIR/" --yes
