7aa90fde1cbace42ac7e34003c62acdacb45cd3c
Ashley Davis <ashley@codecapers.com.au>
2026-01-23 06:45:42 +1000

    Moved the Navbar to its own component.

---
+++ packages/user-interface/src/components/navbar.tsx
@@ -0,0 +1,276 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { Spinner } from "./spinner";
+import Dropdown from '@mui/joy/Dropdown';
+import MenuButton from '@mui/joy/MenuButton';
+import IconButton from '@mui/joy/IconButton';
+import MoreVert from '@mui/icons-material/MoreVert';
+import MenuItem from '@mui/joy/MenuItem';
+import Menu from '@mui/joy/Menu';
+import ListDivider from '@mui/joy/ListDivider';
+import ListSubheader from "@mui/joy/ListSubheader";
+import Delete from "@mui/icons-material/Delete";
+import Input from "@mui/joy/Input/Input";
+import { useTheme } from "@mui/joy/styles/ThemeProvider";
+import classNames from "classnames";
+
+export interface INavbarProps {
+    //
+    // Set to true to open the sidebar.
+    //
+    sidebarOpen: boolean;
+    setSidebarOpen: (open: boolean) => void;
+
+    //
+    // Set to true to open the search input.
+    //
+    openSearch: boolean;
+    setOpenSearch: (open: boolean) => void;
+
+    //
+    // The search currently being typed by the user.
+    //
+    searchInput: string;
+    setSearchInput: (input: string) => void;
+
+    //
+    // Number of sorted items.
+    //
+    sortedItemsCount: number;
+
+    //
+    // Number of selected items.
+    //
+    selectedItemsCount: number;
+
+    //
+    // Clears the multi-selection.
+    //
+    clearMultiSelection: () => void;
+
+    //
+    // True if data is loading.
+    //
+    isLoading: boolean;
+
+    //
+    // Current database path.
+    //
+    databasePath: string | null;
+
+    //
+    // List of all database paths.
+    //
+    dbs: string[];
+
+    //
+    // Moves selected items to the specified database.
+    //
+    onMoveSelectedToDatabase: (databaseid: string) => Promise<void>;
+
+    //
+    // Opens the delete confirmation dialog.
+    //
+    setDeleteConfirmationOpen: (open: boolean) => void;
+
+    //
+    // Commits the search the user has typed in.
+    //
+    onCommitSearch: () => Promise<void>;
+
+    //
+    // Cancels/closes the search.
+    //
+    onCloseSearch: () => Promise<void>;
+}
+
+//
+// The navbar component for the Photosphere app.
+//
+export function Navbar({
+    sidebarOpen,
+    setSidebarOpen,
+    openSearch,
+    setOpenSearch,
+    searchInput,
+    setSearchInput,
+    sortedItemsCount,
+    selectedItemsCount,
+    clearMultiSelection,
+    isLoading,
+    databasePath,
+    dbs,
+    onMoveSelectedToDatabase,
+    setDeleteConfirmationOpen,
+    onCommitSearch,
+    onCloseSearch,
+}: INavbarProps) {
+    const theme = useTheme();
+
+    return (
+        <div 
+            id="navbar" 
+            className={"select-none " + classNames({ "search": openSearch })}
+            style={{
+                backgroundColor: theme.palette.background.body,
+                color: theme.palette.text.primary,
+            }}
+        >
+            <div className="flex flex-col">
+                <div className="flex flex-row items-center pl-4 pt-3 pb-2">
+                    <button
+                        onClick={() => setSidebarOpen(!sidebarOpen)}
+                    >
+                        <i className="fa-solid fa-bars"></i>
+                    </button>
+
+                    <h1 className="ml-3 sm:ml-4">Photosphere</h1>
+
+                    <button
+                        className="ml-4 mr-1 sm:ml-8 sm:mr-3"
+                        onClick={event => {
+                            setOpenSearch(true);
+                        }}
+                    >
+                        <div className="flex flex-row items-center">
+                            <i className="w-5 text-center fa-solid fa-search"></i>
+                            <div className="hidden sm:block ml-2">Search</div>
+                        </div>
+                    </button>
+
+                    <NavLink
+                        className="mr-1 sm:mr-3"
+                        to="/cloud"
+                    >
+                        <div className="flex flex-row items-center">
+                            <i className="w-5 text-center fa-solid fa-cloud"></i>
+                            <div className="hidden sm:block ml-2">Cloud</div>
+                        </div>
+                    </NavLink>
+
+                    <NavLink
+                        className="mr-1 sm:mr-3"
+                        to="/about"
+                    >
+                        <div className="flex flex-row items-center">
+                            <i className="w-5 text-center fa-solid fa-circle-info"></i>
+                            <div className="hidden sm:block ml-2">About</div>
+                        </div>
+                    </NavLink>                        
+
+                    <div className="ml-auto"></div>
+
+                    {(isLoading)
+                        && <div className="flex flex-row items-center ml-1 mr-2">
+                            <span className="text-sm hidden sm:block mr-1">Loading</span>
+                            <div className="mx-1 sm:mx-2">
+                                <Spinner show={true} />
+                            </div>
+                        </div>
+                    }
+
+                    <div
+                        className="flex flex-row items-center mr-2 text-xs sm:text-sm"
+                    >
+                        {selectedItemsCount > 0 
+                            && <div className="flex flex-row items-center">
+                                <button
+                                    className="w-6 text-sm"
+                                    onClick={clearMultiSelection}
+                                >
+                                    <i className="fa-solid fa-close"></i>
+                                </button>                                    
+                                {selectedItemsCount} selected
+                            </div>
+                            || <div>{sortedItemsCount} photos</div>
+                        }
+                        
+                    </div>
+
+                    {(selectedItemsCount > 0)
+                        && <Dropdown>
+                            <MenuButton
+                                sx={{
+                                    mr: 1,
+                                }}                            
+                                slots={{ root: IconButton }}
+                                slotProps={{ root: { variant: 'soft', color: 'neutral' } }}
+                            >
+                                <MoreVert />
+                            </MenuButton>
+                            <Menu placement="bottom-end">
+                                {selectedItemsCount > 0
+                                    && <>
+                                        <ListSubheader>MOVE TO</ListSubheader>
+                                        {dbs.map(dbPath => {
+                                            if (dbPath === databasePath) {
+                                                return null; // Don't show the current database.
+                                            }
+                                            return (
+                                                <MenuItem 
+                                                    key={dbPath}
+                                                    onClick={() => onMoveSelectedToDatabase(dbPath)}
+                                                >
+                                                    {dbPath}                                        
+                                                </MenuItem>
+                                            );
+                                        })}
+                                        <ListDivider />
+                                        <MenuItem
+                                            color="danger"
+                                            onClick={() => setDeleteConfirmationOpen(true)}
+                                        >
+                                            <Delete />
+                                            Delete {selectedItemsCount} assets
+                                        </MenuItem>                                        
+                                        <ListDivider />
+                                    </>
+                                }                                    
+                            </Menu>
+                        </Dropdown>
+                    }
+                </div>
+
+                {openSearch
+                    && <div className="flex flex-row items-center pl-4 pr-1">
+                        <div>
+                            <i className="fa-solid fa-search"></i>
+                        </div>
+                        <Input
+                            size="sm"
+                            autoFocus 
+                            className="flex-grow ml-4 outline-none"
+                            placeholder="Type your search and press enter"
+                            value={searchInput} 
+                            onChange={event => {
+                                setSearchInput(event.target.value);
+                            }}
+                            onKeyDown={async event => {
+                                if (event.key === "Enter") {
+                                    //
+                                    // Commits the search.
+                                    //
+                                    await onCommitSearch();
+                                }
+                                else if (event.key === "Escape") {
+                                    //
+                                    // Cancels the search.
+                                    //
+                                    await onCloseSearch();
+                                }
+                            }}
+                        />
+                        <button
+                            className="w-10 text-xl"
+                            onClick={onCloseSearch}
+                        >
+                            <i className="fa-solid fa-close"></i>
+                        </button>
+                    </div>
+                }                    
+            </div>
+            
+        </div>
+    );
+}
+
--- packages/user-interface/src/main.tsx
+++ packages/user-interface/src/main.tsx
@@ -6,24 +6,15 @@ import { useGallery } from "./context/gallery-context";
 import classNames from "classnames";
 import { useApp } from "./context/app-context";
 import { usePlatform } from "./context/platform-context";
-import Dropdown from '@mui/joy/Dropdown';
-import MenuButton from '@mui/joy/MenuButton';
-import IconButton from '@mui/joy/IconButton';
-import MoreVert from '@mui/icons-material/MoreVert';
-import MenuItem from '@mui/joy/MenuItem';
-import Menu from '@mui/joy/Menu';
-import ListDivider from '@mui/joy/ListDivider';
-import ListSubheader from "@mui/joy/ListSubheader";
 import { useAssetDatabase } from "./context/asset-database-source";
 import { FullscreenSpinner } from "./components/full-screen-spinnner";
-import Delete from "@mui/icons-material/Delete";
 import { DeleteConfirmationDialog } from "./components/delete-confirmation-dialog";
 import { CssVarsProvider, useColorScheme } from "@mui/joy/styles/CssVarsProvider";
 import { ModeToggle } from "./components/mode-toggle";
 import { useTheme } from "@mui/joy/styles/ThemeProvider";
 import Drawer from "@mui/joy/Drawer/Drawer";
 import { Sidebar } from "./components/sidebar";
-import Input from "@mui/joy/Input/Input";
+import { Navbar } from "./components/navbar";
 import { Fps } from "./components/fps";
 import { AboutPage } from "./pages/about";
 
@@ -273,169 +264,24 @@ function __Main({ isMobile = false }: IMainProps) {
    
     return (
         <>
-            <div 
-                id="navbar" 
-                className={"select-none " + (openSearch ? "search": "")} 
-                style={{
-                    backgroundColor: theme.palette.background.body,
-                    color: theme.palette.text.primary,
-                }}
-                >
-                <div className="flex flex-col">
-                    <div className="flex flex-row items-center pl-4 pt-3 pb-2">
-                        <button
-                            onClick={() => setSidebarOpen(!sidebarOpen)}
-                            >
-                            <i className="fa-solid fa-bars"></i>
-                        </button>
-
-                        <h1 className="ml-3 sm:ml-4">Photosphere</h1>
-
-                        <button
-                            className="ml-4 mr-1 sm:ml-8 sm:mr-3"
-                        	onClick={event => {
-                            	setOpenSearch(true);
-                            }}
-                            >
-                            <div className="flex flex-row items-center">
-                                <i className="w-5 text-center fa-solid fa-search"></i>
-                                <div className="hidden sm:block ml-2">Search</div>
-                            </div>
-                        </button>
-
-                        <NavLink
-                            className="mr-1 sm:mr-3"
-                            to="/cloud"
-                            >
-                            <div className="flex flex-row items-center">
-                                <i className="w-5 text-center fa-solid fa-cloud"></i>
-                                <div className="hidden sm:block ml-2">Cloud</div>
-                            </div>
-                        </NavLink>
-
-                        <NavLink
-                            className="mr-1 sm:mr-3"
-                            to="/about"
-                            >
-                            <div className="flex flex-row items-center">
-                                <i className="w-5 text-center fa-solid fa-circle-info"></i>
-                                <div className="hidden sm:block ml-2">About</div>
-                            </div>
-                        </NavLink>                        
-
-                        <div className="ml-auto"></div>
-
-                        {(isLoading)
-                            && <div className="flex flex-row items-center ml-1 mr-2">
-                                <span className="text-sm hidden sm:block mr-1">Loading</span>
-                                <div className="mx-1 sm:mx-2">
-                                    <Spinner show={true} />
-                                </div>
-                            </div>
-                        }
-
-                        <div
-                            className="flex flex-row items-center mr-2 text-xs sm:text-sm"
-                            >
-                            {selectedItems.size > 0 
-                                && <div className="flex flex-row items-center">
-                                    <button
-                                        className="w-6 text-sm"
-                                        onClick={clearMultiSelection}
-                                        >
-                                        <i className="fa-solid fa-close"></i>
-                                    </button>                                    
-                                    {selectedItems.size} selected
-                                </div>
-                                || <div>{sortedItems().length} photos</div>
-                            }
-                            
-                        </div>
-
-                        {(selectedItems.size > 0)
-                            && <Dropdown>
-                                <MenuButton
-                                    sx={{
-                                        mr: 1,
-                                    }}                            
-                                    slots={{ root: IconButton }}
-                                    slotProps={{ root: { variant: 'soft', color: 'neutral' } }}
-                                    >
-                                    <MoreVert />
-                                </MenuButton>
-                                <Menu placement="bottom-end">
-                                    {selectedItems.size > 0
-                                        && <>
-                                            <ListSubheader>MOVE TO</ListSubheader>
-                                            {dbs.map(dbPath => {
-                                                if (dbPath === databasePath) {
-                                                    return null; // Don't show the current database.
-                                                }
-                                                return (
-                                                    <MenuItem 
-                                                        key={dbPath}
-                                                        onClick={() => onMoveSelectedToDatabase(dbPath)}
-                                                        >
-                                                        {dbPath}                                        
-                                                    </MenuItem>
-                                                );
-                                            })}
-                                            <ListDivider />
-                                            <MenuItem
-                                                color="danger"
-                                                onClick={() => setDeleteConfirmationOpen(true)}
-                                                >
-                                                <Delete />
-                                                Delete {selectedItems.size} assets
-                                            </MenuItem>                                        
-                                            <ListDivider />
-                                        </>
-                                    }                                    
-                                </Menu>
-                            </Dropdown>
-                        }
-                    </div>
-
-                    {openSearch
-                        && <div className="flex flex-row items-center pl-4 pr-1">
-                            <div>
-                                <i className="fa-solid fa-search"></i>
-                            </div>
-                            <Input
-                                size="sm"
-                                autoFocus 
-                                className="flex-grow ml-4 outline-none"
-                                placeholder="Type your search and press enter"
-                                value={searchInput} 
-                                onChange={event => {
-                                    setSearchInput(event.target.value);
-                                }}
-                                onKeyDown={async event => {
-                                    if (event.key === "Enter") {
-                                        //
-                                        // Commits the search.
-                                        //
-                                        await onCommitSearch();
-                                    }
-                                    else if (event.key === "Escape") {
-                                        //
-                                        // Cancels the search.
-                                        //
-                                        await onCloseSearch();
-                                    }
-                                }}
-                                />
-                            <button
-                                className="w-10 text-xl"
-                            	onClick={onCloseSearch}
-                                >
-                                <i className="fa-solid fa-close"></i>
-                            </button>
-                        </div>
-                    }                    
-                </div>
-                
-            </div>
+            <Navbar
+                sidebarOpen={sidebarOpen}
+                setSidebarOpen={setSidebarOpen}
+                openSearch={openSearch}
+                setOpenSearch={setOpenSearch}
+                searchInput={searchInput}
+                setSearchInput={setSearchInput}
+                sortedItemsCount={sortedItems().length}
+                selectedItemsCount={selectedItems.size}
+                clearMultiSelection={clearMultiSelection}
+                isLoading={isLoading}
+                databasePath={databasePath}
+                dbs={dbs}
+                onMoveSelectedToDatabase={onMoveSelectedToDatabase}
+                setDeleteConfirmationOpen={setDeleteConfirmationOpen}
+                onCommitSearch={onCommitSearch}
+                onCloseSearch={onCloseSearch}
+            />
 
             <Drawer 
                 open={sidebarOpen} 
